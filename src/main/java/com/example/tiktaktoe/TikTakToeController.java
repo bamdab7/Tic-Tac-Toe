81df9f5395e25b36be0354db2e035ef1feb65f50
nekoo <nereapena42gmail.com>
2022-12-10 00:55:34 +0100

    Declarando variables en el controlador

--- src/main/java/com/example/tiktaktoe/TikTakToeController.java
+++ src/main/java/com/example/tiktaktoe/TikTakToeController.java
@@ -1,14 +1,26 @@
 package com.example.tiktaktoe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 public class TikTakToeController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    public Text winnerText;
+
+    public void restartGame(ActionEvent actionEvent) {
+        
     }
 }
